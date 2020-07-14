292f8f38e531ef4526f3851de807e835fb82a600
Abdul <abdul.ismail06@gmail.com>
2020-07-14 09:46:03 +0100

    bot

--- src/Script/Thiever.java
+++ src/Script/Thiever.java
@@ -26,7 +26,13 @@ public class Thiever extends Script implements MouseListener {
     private float moneyMade = 0;
     private String status = "Script Started";
 
+    // discord bot
+    public static boolean sendMessages = true;
+    public static boolean pauseScript = false;
+    public static String lastSenderName = "";
+
     public void onExecute() {
+        try { new Bot(); } catch (LoginException ignored) { }
         ctx.updateStatus("Started AIO Thiever");
         timeBegan = System.currentTimeMillis();
         showPaint = true;
@@ -34,42 +40,44 @@ public class Thiever extends Script implements MouseListener {
 
     @Override
     public void onProcess() {
-        SimpleNpc npc = ctx.npcs.populate().filter(3104, 3010, 3079, 3085).nearest().next();
+        if (!pauseScript) {
+            SimpleNpc npc = ctx.npcs.populate().filter(3104, 3010, 3079, 3085).nearest().next();
 
-        if (npc != null && npc.validateInteractable() && !stunned) {
-            status = "Pickpocket";
-            npc.click("Pickpocket");
-            ctx.sleepCondition(() -> ctx.players.getLocal().getAnimation() != -1, 300);
-        }
-
-        if(!ctx.inventory.populate().filter(22529, 22521, 22526).isEmpty()) {
-            if(ctx.inventory.populate().filter(22529, 22521, 22526).next().getQuantity() > 10) {
-                status = "Opening coin pouch";
-                ctx.sleep(1000);
-                ctx.inventory.populate().filter(22529, 22521, 22526).next().click(0);
+            if (npc != null && npc.validateInteractable() && !stunned) {
+                status = "Pickpocket";
+                npc.click("Pickpocket");
+                ctx.sleepCondition(() -> ctx.players.getLocal().getAnimation() != -1, 300);
             }
-        }
 
-        if (!ctx.inventory.populate().filter("Sardine", "Trout", "Salmon", "Tuna", "Lobster", "Swordfish", "Monkfish", "Shark").isEmpty()) {
-            if(ctx.players.getLocal().getHealth() < 15) {
-                status = "Eating food";
-                ctx.sleep(1000);
-                ctx.inventory.populate().filter("Sardine", "Trout", "Salmon", "Tuna", "Lobster", "Swordfish", "Monkfish", "Shark").next().click(0);
+            if(!ctx.inventory.populate().filter(22529, 22521, 22526).isEmpty()) {
+                if(ctx.inventory.populate().filter(22529, 22521, 22526).next().getQuantity() > 10) {
+                    status = "Opening coin pouch";
+                    ctx.sleep(1000);
+                    ctx.inventory.populate().filter(22529, 22521, 22526).next().click(0);
+                }
             }
-        } else {
-            ctx.sleep(2000);
-            ctx.sendLogout();
-        }
 
-        if (stunned) {
-            ctx.sleep(3000);
-            stunned = false;
+            if (!ctx.inventory.populate().filter("Sardine", "Trout", "Salmon", "Tuna", "Lobster", "Swordfish", "Monkfish", "Shark").isEmpty()) {
+                if(ctx.players.getLocal().getHealth() < 15) {
+                    status = "Eating food";
+                    ctx.sleep(1000);
+                    ctx.inventory.populate().filter("Sardine", "Trout", "Salmon", "Tuna", "Lobster", "Swordfish", "Monkfish", "Shark").next().click(0);
+                }
+            } else {
+                ctx.sleep(2000);
+                ctx.sendLogout();
+            }
+
+            if (stunned) {
+                ctx.sleep(3000);
+                stunned = false;
+            }
         }
     }
 
     @Override
     public void onTerminate() {
-
+        Bot.jda.shutdownNow();
     }
 
     @Override
@@ -80,6 +88,22 @@ public class Thiever extends Script implements MouseListener {
             if (msg.contains("you have been stunned")) {
                 stunned = true;
                 status = "Stunned";
+            }
+        }
+
+        if (e.getType() == ChatMessageType.PUBLICCHAT && sendMessages) {
+            String message = e.getMessage();
+            String sender = e.getName();
+            if (!sender.equalsIgnoreCase("extreme")) {
+                // regex to replace players with donor/mod signs beside the name
+                sender = sender.replaceAll("<[^>]*>", "🔹️");
+
+                if (lastSenderName.equals(sender)) {
+                    new WebHook(sender, message, true);
+                } else {
+                    lastSenderName = sender;
+                    new WebHook(sender, message, false);
+                }
             }
         }
     }
@@ -122,6 +146,32 @@ public class Thiever extends Script implements MouseListener {
         long minutes = (duration / (1000 * 60) % 60);
         long hours = (duration / (1000 * 60 * 60) % 24);
         return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+    }
+
+    public static void sendMessage(String message) {
+        ClientContext.getAPI().keyboard.sendKeys(message);
+    }
+
+    public static void getPlayers() {
+        SimplePlayerQuery<SimplePlayer> players = ClientContext.getAPI().players.populate();
+        if (players.size() == 0) {
+            new WebHook("No players nearby!", false);
+        } else {
+            for (SimplePlayer player: players) {
+                new WebHook("", player.getName(), false);
+            }
+        }
+    }
+
+    public static void getStaffs() {
+        SimplePlayerQuery<SimplePlayer> staffs = ClientContext.getAPI().antiBan.nearbyStaff();
+        if (staffs.size() == 0) {
+            new WebHook("No staff nearby!", false);
+        } else {
+            for (SimplePlayer staff: staffs) {
+                new WebHook("", staff.getName(), false);
+            }
+        }
     }
 
     @Override
